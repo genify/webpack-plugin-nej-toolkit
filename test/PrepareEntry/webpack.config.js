6df60d8e4682e6128a57cc7b2456ad2a6d86a07d
genify <genify@gmail.com>
2018-08-01 15:25:47 +0800

    @dev test case for entry plugin

--- test/PrepareEntry/webpack.config.js
+++ test/PrepareEntry/webpack.config.js
@@ -1,5 +1,5 @@
 let path = require('path');
-let NEJPluginPrepareEntry = require('../../lib/plugins/PrepareEntry');
+let NEJPluginPrepareEntry = require('../../src/plugins/PrepareEntry');
 
 module.exports = {
     entry: path.resolve(__dirname,'release.conf'),
